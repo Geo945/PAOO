763df7bd0e421b2c25d5dc3d92e9a872c0dd8356
george <george.mirulescu@doctarigroup.com>
2022-10-13 16:13:36 +0300

    setup

---
+++ saptamana3/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mirulescu George on 13.10.2022.
+//
+
+#include "main.h"
---
+++ saptamana3/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Mirulescu George on 13.10.2022.
+//
+
+#ifndef PAOO_MAIN_H
+#define PAOO_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PAOO_MAIN_H
